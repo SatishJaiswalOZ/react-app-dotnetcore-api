d6758864ce5798e9d6115324818fae5190b09468
Satish Kumar Jaiswal <satish.131326.pr@gmail.com>
2020-01-13 04:20:41 +1100

    Lets define Interfaces: IProvider & IExtractEngine
    Testing framework to be used: xunit

---
+++ bet-easy-dot-net-core/dotnet-code-challenge/HorseStatsSubSystem/Extractors/IExtractEngine.cs
@@ -0,0 +1,20 @@
+﻿namespace dotnet_code_challenge
+{
+    public interface IExtractEngine
+    {
+        /// <summary>
+        /// Perform file extraction
+        /// </summary>
+        /// <param name="fileWithFullPath"></param>
+        /// <returns>Type Metting</returns>
+        Meeting Extract(string fileWithFullPath);
+
+        /// <summary>
+        /// Checks whether provided file is supported or not. 
+        /// File check can be much advance to verify various ways to confirm if the file content is actually as per extension name i.e. MIME Type
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns>Confirms if supported.</returns>
+        bool EngineSupported(string fileName);
+    }
+}
---
+++ bet-easy-dot-net-core/dotnet-code-challenge/HorseStatsSubSystem/Provider/IProvider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace dotnet_code_challenge
+{
+    public interface IProvider
+    {
+        /// <summary>
+        /// Provide collection of Horse type extracting from provided input supported files.
+        /// Internally it uses the ExtractEngines.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns>IEnumerable<Horse></returns>
+        IEnumerable<Horse> Provide(string source);
+    }
+}

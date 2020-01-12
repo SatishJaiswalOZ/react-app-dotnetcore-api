9a47f4288098a8124ae0b043a5d777e55b7fbc5c
Satish Kumar Jaiswal <satish.131326.pr@gmail.com>
2020-01-13 04:56:48 +1100

    File Extraction engine. Better error handling can be constructed. Supports Strategy Pattern via detected if the file format is acceptable or not.

---
+++ bet-easy-dot-net-core/dotnet-code-challenge/HorseStatsSubSystem/Extractors/ExtractEngineJSON.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace dotnet_code_challenge
+{
+    public class ExtractEngineJSON : IExtractEngine
+    {
+        public Meeting Extract(string fileWithFullPath)
+        {
+            using (StreamReader reader = File.OpenText(fileWithFullPath))
+            {
+                dynamic data = JsonConvert.DeserializeObject(reader.ReadToEnd());
+                if (!ReferenceEquals(null, data))
+                {
+                    if (data.RawData is object)
+                    {
+                        IEnumerable<dynamic> markets = data.RawData.Markets;
+                        if (data.RawData.Markets is object)
+                        {
+                            return new Meeting
+                            {
+                                MeetingId = data.RawData.Id,
+                                Races = markets.Select((market, index) =>
+                                {
+                                    if (market.Selections is object)
+                                    {
+                                        IEnumerable<dynamic> selections = market.Selections;
+                                        return new Race
+                                        {
+                                            Number = index + 1,
+                                            Horses = selections.Select(selection =>
+                                            {
+                                                return new Horse
+                                                {
+                                                    Name = (selection.Tags is object) ? selection.Tags.name: string.Empty,
+                                                    Price = decimal.Parse((string)selection.Price)
+                                                };
+                                            })
+                                        };
+                                    }
+                                    return null;
+                                })
+                            };
+                        }
+                    }
+                }
+                return null;
+            }
+        }
+
+        public bool EngineSupported(string fileName)
+        {
+            return fileName.ToLower().EndsWith(".json", StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
---
+++ bet-easy-dot-net-core/dotnet-code-challenge/HorseStatsSubSystem/Extractors/ExtractEngineXML.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace dotnet_code_challenge
+{
+    public class ExtractEngineXML : IExtractEngine
+    {
+        public Meeting Extract(string fileWithFullPath)
+        {
+            try
+            {
+                var doc = XDocument.Load(fileWithFullPath);
+                return new Meeting
+                {
+                    MeetingId = doc.Descendants("Meetingid").Single().Value,
+                    Races = doc.Descendants("race").Select((race) =>
+                    {
+                        var prices = race.Elements("prices").Single().Descendants("horse");
+                        return new Race
+                        {
+                            Number = int.Parse(race.Attribute("number").Value),
+                            Horses = race.Elements("horses").Single().Elements("horse").Select(horse =>
+                                {
+
+                                    return new Horse
+                                    {
+                                        Name = horse.Attribute("name").Value,
+                                        Price = decimal.Parse(prices.First(p => p.Attribute("number").Value == horse.Elements("number").Single().Value).Attribute("Price").Value)
+                                    };
+
+                                })
+                        };
+
+                    })
+                };
+            }
+            catch (InvalidOperationException)
+            {
+                //For this excercise engulf issues but it has to be addressed to inform user.
+                return null;
+            }
+        }
+
+        public bool EngineSupported(string fileName)
+        {
+            return fileName.ToLower().EndsWith(".xml", StringComparison.OrdinalIgnoreCase);
+        }
+
+    }
+}

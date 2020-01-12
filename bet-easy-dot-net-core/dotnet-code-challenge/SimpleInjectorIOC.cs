32985d129cd26c7991c8d1fb895cf4617c209209
Satish Kumar Jaiswal <satish.131326.pr@gmail.com>
2020-01-13 04:15:23 +1100

    Created class as a placeholder for IOC - Nuget Package: Simple Injector is installed and anytime it can be configured to register required IOC/Dependency Injections.
    IOC as of now:
    Logger
    Provider

---
+++ bet-easy-dot-net-core/dotnet-code-challenge/SimpleInjectorIOC.cs
@@ -0,0 +1,13 @@
+﻿using NLog;
+using System.Collections.Generic;
+
+namespace dotnet_code_challenge
+{
+    static class SimpleInjectorIOC
+    {
+        // Below services can be injected in any system via Simple Injector IOC or similar tools/API and remain Singleton & further serves as Dependency Injections
+        // This is a temporary placeholder
+        internal static readonly Logger Logger = LogManager.GetLogger("fileLogger");
+        internal static readonly IProvider Provider = new ExtractProvider(new List<IExtractEngine> { new ExtractEngineJSON(), new ExtractEngineXML() });
+    }
+}

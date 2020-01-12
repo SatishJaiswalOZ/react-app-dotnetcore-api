e00011e8ce892bbeb6a2d7a277cc4410775878a8
Satish Kumar Jaiswal <satish.131326.pr@gmail.com>
2020-01-13 04:28:38 +1100

    Strategy Pattern helps to identify file extractions engine hiding the implementation from the client. Also, DI helps in loosely coupled classes.

---
+++ bet-easy-dot-net-core/dotnet-code-challenge/HorseStatsSubSystem/Provider/ExtractProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace dotnet_code_challenge
+{
+    public class ExtractProvider : IProvider
+    {
+        private readonly IEnumerable<IExtractEngine> extractEngine;
+
+        public ExtractProvider(IEnumerable<IExtractEngine> extractEngine)
+        {
+            //Dependency Injection
+            this.extractEngine = extractEngine;
+        }
+
+        public IEnumerable<Horse> Provide(string fileWithFullPath)
+        {
+            //Strategy pattern
+            IExtractEngine supportedExtractEngine = extractEngine.FirstOrDefault(x => x.EngineSupported(fileWithFullPath));
+
+            if (supportedExtractEngine == null)
+            {
+                throw new Exception();
+            }
+            return supportedExtractEngine.Extract(fileWithFullPath).Races.SelectMany(x => x.Horses).OrderBy(x => x.Price);
+        }
+    }
+}

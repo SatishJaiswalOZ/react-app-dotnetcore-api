e42189b9c5c9005cdbdd3b09f95ac9fc3108dfe8
Satish Kumar Jaiswal <satish.131326.pr@gmail.com>
2020-01-13 04:31:03 +1100

    Program.cs i.e. client just invokes GetHorsesStats providing filepath & certain dependencies from IOC/Simple Injector can be used ideally. Once full collection is received and sorted in ascending order of price, further print to console can be done.

---
+++ bet-easy-dot-net-core/dotnet-code-challenge/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace dotnet_code_challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("An application which outputs the horse names in price ascending order.\n\nLet's start...\n");
+            var results = new HorseStats(
+                SimpleInjectorIOC.Logger,
+                SimpleInjectorIOC.Provider).
+                GetHorsesStats(string.Concat(AppDomain.CurrentDomain.BaseDirectory, "FeedData"));
+            Console.WriteLine(" Results: \n");
+
+            if (results != null)
+            {
+                for (int i = 0; i < results.Count; i++)
+                {
+                    var horse = results[i];
+                    Console.WriteLine($" {horse.Name,-50} {horse.Price:C} (AUD)");
+                }
+            }
+            Console.WriteLine(results == null ? "\nSorry! Something went wrong. Please refer log file." : "\nOperation completed successfully. Thanks!");
+            Console.ReadKey();
+        }
+    }
+}
